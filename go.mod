95874a9f93d59d136310a0ded4ccc4c71b39f9c5
Izaak Dale <izaakdaledev@gmail.com>
2023-11-20 13:23:31 -0800

    removing lambda func test

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/izaakdale/service-barebones
 
 go 1.21
+
+require github.com/izaakdale/lib v0.0.0-20230426101354-d903fabb3bb7
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/izaakdale/lib/response"
+	"github.com/izaakdale/lib/router"
+	"github.com/izaakdale/lib/server"
 )
 
 func main() {
-	fmt.Println("testing lambda func!")
-	// s, err := server.New(
-	// 	"service-barebones",
-	// 	router.New(
-	// 		router.WithRoute(
-	// 			http.MethodGet,
-	// 			"/_/alive",
-	// 			func(w http.ResponseWriter, r *http.Request) {
-	// 				response.WriteJson(w, http.StatusOK, "alive")
-	// 				return
-	// 			},
-	// 		),
-	// 	),
-	// 	server.WithHost(os.Getenv("HOST")),
-	// 	server.WithPort(os.Getenv("PORT")),
-	// 	server.WithTimeoutHandler(time.Second, "timedout!"),
-	// )
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// s.Run()
+	s, err := server.New(
+		"service-barebones",
+		router.New(
+			router.WithRoute(
+				http.MethodGet,
+				"/_/alive",
+				func(w http.ResponseWriter, r *http.Request) {
+					response.WriteJson(w, http.StatusOK, "alive")
+					return
+				},
+			),
+		),
+		server.WithHost(os.Getenv("HOST")),
+		server.WithPort(os.Getenv("PORT")),
+		server.WithTimeoutHandler(time.Second, "timedout!"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	s.Run()
 }
